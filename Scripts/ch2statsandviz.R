7ee630284b6d3cb719bb55e004824060f15aad9b
Jenna Braun <braunj@yorku.ca>
2018-03-22 20:57:15 -0400

    update 3.22

--- Scripts/ch2statsandviz.R
+++ Scripts/ch2statsandviz.R
@@ -1,8 +1,8 @@
 library(dplyr)
 library(ggplot2)
 
-byobs <- read.csv("Data/visitation/byobs_cleaned.csv")
-byrep <- read.csv("Data/visitation/byrep_cleaned.csv")
+byobs <- read.csv("byobs_cleaned.csv")
+byrep <- read.csv("byrep_cleaned.csv")
 str(byrep)
 
 shapiro.test(byrep$visits.per.hour)
@@ -45,3 +45,17 @@ ggplot(data = counts, aes(microsite, mean.prop.fl)) + geom_boxplot() + xlab("Mic
 ggplot(data = counts, aes(microsite, flowers.visited)) + geom_boxplot() + xlab("Microsite") + ylab("Flowers visited per hour") + theme_Publication() + facet_grid(~flowering)
 
 ggplot(data = all.data, aes(treatment, visits.per.hour)) + geom_boxplot() + xlab("Microsite") + ylab("Flower visits per hour")+ facet_grid(~flowering)
+
+
+#try out glm
+g1 <- glm(data = byrep, family = quasipoisson (link = "log"), visits.per.hour ~ treatment + flowering)
+g1
+summary(g1)
+
+g2 <- glm(data = byrep, family = quasipoisson (link = "log"), visits.per.hour ~ treatment * flowering)
+summary(g2)
+g2
+
+g3 <- glm(data = byrep, family = quasipoisson (link = "log"), visits.per.hour ~ treatment + flowering + understory.richness + het.annual.floral.density + het.shrub.blooming.neighbours + understory.assoc)
+summary(g3)
+g3          
